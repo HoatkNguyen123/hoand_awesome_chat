780f935bca81d3047773354d115ac77bafd5b27c
=Nguyen Duy Hoa <=duyhoatk97@gmail.com>
2020-08-18 23:41:05 +0700

    send email confirm account

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -21,7 +21,7 @@ let postRegister = async (req, res) => {
     return res.redirect("/login-register");
   }
   try {
-    let createUserSuccess = await auth.register(req.body.email, req.body.gender, req.body.password);
+    let createUserSuccess = await auth.register(req.body.email, req.body.gender, req.body.password, req.protocol, req.get("host"));
     successArr.push(createUserSuccess);
     req.flash("success", successArr);
     return res.redirect("/login-register");
@@ -35,9 +35,24 @@ let postRegister = async (req, res) => {
 
 };
 
+let verifyAccount = async (req, res) => {
+  let errorArr = [];
+  let successArr = [];
+  try {
+    let verifySuccess = await auth.verifyAccount(req.params.token);
+    successArr.push(verifySuccess);
+    req.flash("success", successArr);
+    return res.redirect("/login-register");
+  } catch (error) {
+    errorArr.push(error);
+    req.flash("errors", errorArr);
+    return res.redirect("/login-register");
+  }
+};
 
 
 module.exports = {
   getLoginRegister: getLoginRegister,
   postRegister: postRegister,
+  verifyAccount: verifyAccount
 };
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,21 +1,19 @@
 import UserModel from "../models/userModel";
 import bcrypt from "bcrypt";
 import uuidv4 from "uuid/v4";
-import { transErrors, tranSuccess } from "../../lang/vi";
-
+import { transErrors, tranSuccess, tranMail } from "../../lang/vi";
+import sendMail from "../config/mailer";
 
 let saltRounds = 7;
 
-let register =  (email, gender, password) => {
-  return new Promise(async(resolve, reject) => {
+let register = (email, gender, password, protocol, host) => {
+  return new Promise(async (resolve, reject) => {
     let userByEmail = await UserModel.findByEmail(email);
     if (userByEmail) {
-      if(userByEmail.deletedAt != null)
-      { 
+      if (userByEmail.deletedAt != null) {
         return reject(transErrors.account_remove);
       }
-      if(!userByEmail.local.isActive)
-      { 
+      if (!userByEmail.local.isActive) {
         return reject(transErrors.account_not_active);
       }
       return reject(transErrors.account_in_user);
@@ -34,11 +32,33 @@ let register =  (email, gender, password) => {
     };
 
     let user = await UserModel.createNew(userItem);
-    resolve(tranSuccess.userCreated(user.local.email));
+    let linkVerify = `${protocol}://${host}/verify/${user.local.verifyToken}`;
+    //send email
+    sendMail(email, tranMail.subject, tranMail.template(linkVerify))
+      .then((success) => {
+        resolve(tranSuccess.userCreated(user.local.email));
+      })
+      .catch(async (error) => {
+        await UserModel.removeById(user._id);
+        console.log(error);
+        reject(tranMail.send_failed);
+      });
+
   });
 
 };
 
+let verifyAccount =  (token) => {
+  return new Promise( async (resolve, reject) => {
+    let userByToken = await UserModel.findByToken(token);
+    if(!userByToken){
+      return reject(transErrors.token_undefined);
+    }
+    await UserModel.verify(token);
+    resolve(tranSuccess.account_active);
+  });
+}
 module.exports = {
   register: register,
+  verifyAccount: verifyAccount
 }
