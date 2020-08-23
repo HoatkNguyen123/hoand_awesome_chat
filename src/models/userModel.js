2055b628f4a84af7c0e5e17e32b2ccf00ca55b3c
=Nguyen Duy Hoa <=duyhoatk97@gmail.com>
2020-08-23 14:24:28 +0700

    Update password

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -68,6 +68,10 @@ UserSchema.statics = {
   updateUser(id, item) {
     return this.findByIdAndUpdate(id, item).exec();
   },
+
+  updatePassword(id, hashedPassword) {
+    return this.findByIdAndUpdate(id, {"local.password": hashedPassword}).exec();
+  }
 };
 
 UserSchema.methods = {
