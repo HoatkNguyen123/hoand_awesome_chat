31783408b9e4460471ed9d939e5df22a858695f7
=Nguyen Duy Hoa <=duyhoatk97@gmail.com>
2020-11-14 20:38:38 +0700

    fix bug

--- src/public/js/readMoreMessages.js
+++ src/public/js/readMoreMessages.js
@@ -1,5 +1,5 @@
 function readMoreMessages(){
-  $(".right .chat").scroll(function(){
+  $(".right .chat").unbind("scroll").on("scroll", function(){
 
     let firstMessage = $(this).find(".bubble:first");
 
